fc7146ad79bfcae95fe177cc569b4b3db51503cc
sscchan <stephen.s.c.chan@gmail.com>
2017-07-13 13:27:30 +0930

    Implemented public method for showing notifications

--- EyeExerciseReminder/src/eyeexercisereminder/SystemTrayMechanism.java
+++ EyeExerciseReminder/src/eyeexercisereminder/SystemTrayMechanism.java
@@ -10,9 +10,6 @@ import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.Image;
 import java.awt.PopupMenu;
-import java.awt.MenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -24,20 +21,27 @@ import javax.imageio.ImageIO;
  * @author stephenchan
  */
 public class SystemTrayMechanism {
-    public SystemTrayMechanism(String applicationTitle, String iconFilePath, PopupMenu popupMenu) {
-        if (SystemTray.isSupported()) {
-            SystemTray systemTray = SystemTray.getSystemTray();
+   
+   private TrayIcon trayIcon;
+   
+   public SystemTrayMechanism(String applicationTitle, String iconFilePath, PopupMenu popupMenu) {
+      if (SystemTray.isSupported()) {
+         SystemTray systemTray = SystemTray.getSystemTray();
             
-            try {
-                Image trayIconImage = ImageIO.read(new File(iconFilePath));
-                TrayIcon trayIcon = new TrayIcon(trayIconImage, applicationTitle, popupMenu);     
-                systemTray.add(trayIcon);
-                
+         try {
+            Image trayIconImage = ImageIO.read(new File(iconFilePath));
+            trayIcon = new TrayIcon(trayIconImage, applicationTitle, popupMenu);     
+            systemTray.add(trayIcon);
+            
             } catch (IOException | AWTException ex) {
                 Logger.getLogger(SystemTrayMechanism.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } else {
-            System.out.println("System Tray Not Supported");
-        }
+      } else {
+         System.out.println("System Tray Not Supported");
+      }
     }
+   
+   public void showNotification(String notificationTitle, String notificationContent) {
+      trayIcon.displayMessage(notificationTitle, notificationContent, TrayIcon.MessageType.INFO);
+   }
 }
