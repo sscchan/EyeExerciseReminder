a5d4e5d14b8bcd08518646f943f2eb932a2a78f2
sscchan <stephen.s.c.chan@gmail.com>
2017-07-13 13:27:02 +0930

    Implemented fixed 10 minute timer for application with notification

--- EyeExerciseReminder/src/eyeexercisereminder/EyeExerciseReminder.java
+++ EyeExerciseReminder/src/eyeexercisereminder/EyeExerciseReminder.java
@@ -5,17 +5,40 @@
  */
 package eyeexercisereminder;
 
+import java.awt.PopupMenu;
+import java.awt.TrayIcon;
+import java.util.Timer;
+import java.util.TimerTask;
 /**
  *
  * @author stephenchan
  */
 public class EyeExerciseReminder {
 
+    final private static String APPLICATION_TITLE = "Eye Exercise Reminder";
+    final private static String SYSTEM_TRAY_ICON_FILEPATH = "./resources/ApplicationSystemTrayIcon.png";
+    final private static long REMINDER_INTERVAL = 10;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //setApplicationToRunInHeadlessMode();
+        
+        SystemTrayPopupMenu systemTrayPopupMenu = new SystemTrayPopupMenu();
+        SystemTrayMechanism systemTrayMechanism = new SystemTrayMechanism(APPLICATION_TITLE, SYSTEM_TRAY_ICON_FILEPATH, systemTrayPopupMenu);
+        
+        Timer reminderTimer = new Timer();
+        reminderTimer.schedule(new TimerTask() {
+           public void run() {
+              systemTrayMechanism.showNotification(APPLICATION_TITLE, "Exercise Your Eyes!");
+           }
+        }, REMINDER_INTERVAL * 60, REMINDER_INTERVAL * 60);
+
+    }
+    
+    private static void setApplicationToRunInHeadlessMode() {
+        // Remove Java "Icon" in OSX Dock      
+        System.setProperty("apple.awt.UIElement", "true");
     }
     
 }
