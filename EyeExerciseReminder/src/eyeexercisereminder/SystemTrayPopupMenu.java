edfbc684d64cdf3038e198dfedab6a3c691f32cc
sscchan <stephen.s.c.chan@gmail.com>
2017-07-13 11:05:33 +0930

    Implement barebone system tray icon menu with exit function

---
+++ EyeExerciseReminder/src/eyeexercisereminder/SystemTrayPopupMenu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eyeexercisereminder;
+
+import java.awt.PopupMenu;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author stephenchan
+ */
+public class SystemTrayPopupMenu extends PopupMenu{
+   public SystemTrayPopupMenu() {
+      super();
+      MenuItem exitMenuItem = new MenuItem("Exit");
+      super.add(exitMenuItem);
+      exitMenuItem.addActionListener((ActionEvent event) -> {
+         System.exit(0);
+      });
+   }
+}
